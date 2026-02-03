418224d7be469272e9a071692cf6fa5083a10154
Wembo XD <github.a084@gmail.com>
2026-02-03 22:58:11 +0400

    Update index.jsud3xhncjn  cnfrnm

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ http.createServer((req, res) => {
 
 // 2. AI CONFIG (Fixed model string)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 let lastCheckedCommentId = null;
 const TARGET_ACCOUNT = "GeminiModel";
 
